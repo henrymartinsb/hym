6c5864000b1dedc104bb049262d48fe0fbd9b50f
Henry Martins <henrymartinsb@icloud.com>
2022-12-20 16:26:35 -0300

    resolve spaces

--- components/message.js
+++ components/message.js
@@ -5,7 +5,7 @@ export default function Message({ children, avatar, username, description }) {
         <span className="text-white text-xs font-extrabold p-1 bg-gradient-to-r from-sky-500 to-indigo-500 rounded-full">🧸</span>
         <h2 className="font-extrabold text-lg font-object">{username}</h2>
       </div>
-      <div className="py-4 break-words text-base indent-7">
+      <div className="py-4 text-base whitespace-pre-line break-words">
         <p>{description}</p>
       </div>
       {children}
